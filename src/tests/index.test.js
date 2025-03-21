b0e724fc2512f37426d30aac500f8eb3e078702b
ravi <rv200414z@gmail.com>
2025-03-21 17:38:34 +0530

    changes

--- src/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("../app"); // Import your Express app
-
-describe("Auth Route Tests", () => {
-  test("GET /api/auth should return 200", async () => {
-    const res = await request(app).get("/api/auth/");
-    expect(res.statusCode).toBe(200);
-  });
-});
